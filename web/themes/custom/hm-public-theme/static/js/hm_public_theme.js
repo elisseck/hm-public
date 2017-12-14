226f55789fa03f2bf0de03fc77cf34d167935f9d
August McKinney <august.mckinney@thirdwavellc.com>
2017-12-14 13:58:56 -0600

    Add back to top button and organize it so it is under the view content

--- web/themes/custom/hm-public-theme/static/js/hm_public_theme.js
+++ web/themes/custom/hm-public-theme/static/js/hm_public_theme.js
@@ -27,3 +27,11 @@ $('.carousel').carousel({
   interval: 5000,
   pause: "false"
 });
+
+$(window).scroll(function() {
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+  $('body,html').animate({
+      scrollTop : 0                       // Scroll to top of body
+  }, 500);
+});
