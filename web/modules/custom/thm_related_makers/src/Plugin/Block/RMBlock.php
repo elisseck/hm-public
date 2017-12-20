1703524b4e8aa9257f135b57f0e5cb5bba163045
Tony Taylor <tony.taylor@thirdwavellc.com>
2017-12-20 11:04:25 -0600

    added pathauto (page alias) module; added related maker full page support

--- web/modules/custom/thm_related_makers/src/Plugin/Block/RMBlock.php
+++ web/modules/custom/thm_related_makers/src/Plugin/Block/RMBlock.php
@@ -19,9 +19,9 @@ class RMBlock extends BlockBase {
    */
   public function build() {
 
-    $vs = thm_related_makers_perform_search();
+    $vs = thm_related_makers_perform_search(null,false, 5);
 
-    //drupal_set_message('sending to twig: ' . print_r($vs['data'], true));
+    drupal_set_message('sending to twig: ' . print_r($vs['data'], true));
 
     $build = [];
     $build['block_related_makers']['#cache']['max-age'] = 0;
