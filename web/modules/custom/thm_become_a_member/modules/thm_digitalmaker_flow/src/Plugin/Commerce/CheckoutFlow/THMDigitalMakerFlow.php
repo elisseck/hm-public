8353612d24b0d399b306863119baa8c9a103b575
Tony Taylor <tony.taylor@thirdwavellc.com>
2018-01-12 16:17:38 -0600

    added digitalmaker custom checkout flow; refactored thm_become_a_member as a module suite

---
+++ web/modules/custom/thm_become_a_member/modules/thm_digitalmaker_flow/src/Plugin/Commerce/CheckoutFlow/THMDigitalMakerFlow.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\thm_digitalmaker_flow\Plugin\Commerce\CheckoutFlow;
+
+use Drupal\commerce_checkout\Plugin\Commerce\CheckoutFlow\CheckoutFlowWithPanesBase;
+
+
+/**
+ * @CommerceCheckoutFlow(
+ *  id = "thm_digitalmaker_checkout_flow",
+ *  label = @Translation("DigitalMaker Checkout Flow"),
+ * )
+ */
+class THMDigitalMakerFlow extends CheckoutFlowWithPanesBase {
+
+  /**
+  * {@inheritdoc}
+  */
+  public function getSteps() {
+    return [
+      'membership_options' => [
+        'label' => $this->t('Membership Options'),
+        'previous_label' => $this->t('Go Back')
+      ],
+      'registration_info' => [
+        'label' => $this->t('Registration Information'),
+        'previous_label' => $this->t('Go Back')
+      ],
+      'payment_method' => [
+        'label' => $this->t('Payment Method'),
+        'previous_label' => $this->t('Go Back')
+      ]
+    ] + parent::getSteps();
+  }
+}
