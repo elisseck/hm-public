7ea3c0e86ce050c2c3007bbc50dbfd3da919390f
Juan Mendez <contact@juanmendez.info>
2018-04-12 15:14:07 -0500

    initial smartphone screen

--- web/themes/custom/hm-public-theme/static/js/hm_public_theme.js
+++ web/themes/custom/hm-public-theme/static/js/hm_public_theme.js
@@ -41,4 +41,76 @@ function scrollStep() {
     // Check if we're at the top already.
     window.scrollTo(0, 0);
 }
-$scrollButton.addEventListener('click', scrollStep);
+
+if( $scrollButton ){
+  $scrollButton.addEventListener('click', scrollStep);
+}
+
+var makeFooterStayBelow = function(){
+
+  var $canvas = $(".dialog-off-canvas-main-canvas").eq(0);
+  var $content = $( '.content-wrapper').eq(0);
+
+  $content.attr( "style", "min-height:''");
+
+  $content.height( "auto" );
+  $canvas.height("auto");
+
+  //sometimes content can be too short..
+  var diff = $(document).height() - $canvas.height();
+
+  if( diff > 0 ) {
+    $content.attr( "style", "min-height:" + ($content.height() + diff) + "px");
+  }
+};
+
+
+var megamenuUtils = function(){
+  var $megamenu = $( ".menu.menu--simple-mega-menu" );
+  var $spans = $( "span", $megamenu );
+
+
+  $spans.on( "click", function(e){
+    var $thisSpan = $(this);
+    var $parent = $thisSpan.parent();
+
+    if( !$parent.hasClass("show") ){
+      $parent.addClass("show");
+    }else{
+      $parent.removeClass("show");
+    }
+
+    //close other menus
+    $spans.each( function(i, span){
+      var $span = $(span);
+      if( $span[0] != $thisSpan[0] ){
+        $span.parent().removeClass( "show" );
+      }
+    });
+  }).focusout(  function(){
+    console.log( "blur me");
+    var $thisSpan = $(this);
+    var $parent = $thisSpan.parent();
+    $parent.removeClass("show");
+  });
+
+  var onResize = function(){
+    if( $(document).width() > 1247 ){
+      $spans.each( function(i, span){
+        $(span).parent().removeClass( "show" );
+      });
+    }
+  }
+
+  $( window ).resize( onResize );
+  onResize();
+}
+
+$(function() {
+  makeFooterStayBelow();
+  megamenuUtils();
+
+  $( window ).resize(function() {
+    makeFooterStayBelow();
+  });
+});
