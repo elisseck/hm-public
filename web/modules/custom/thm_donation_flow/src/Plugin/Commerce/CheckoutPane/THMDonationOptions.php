0d13ab525db2b6fab731b47474ba005afd6c03a1
Tony Taylor <tony.taylor@thirdwavellc.com>
2018-04-23 15:50:59 -0500

    refactored the following:   - donations will auto-fill email if the user is currently logged in;   - the User Profile and Registration Forms are now joined (BasicMaker)   - user matching engine output has been added to twig and modified to only render on the user matching form

--- web/modules/custom/thm_donation_flow/src/Plugin/Commerce/CheckoutPane/THMDonationOptions.php
+++ web/modules/custom/thm_donation_flow/src/Plugin/Commerce/CheckoutPane/THMDonationOptions.php
@@ -66,6 +66,8 @@ class THMDonationOptions extends CheckoutPaneBase {
    */
   public function buildPaneForm(array $pane_form, FormStateInterface $form_state, array &$complete_form) {
 
+    $currentUser = \Drupal::currentUser();
+
     $pane_form['donation_opts'] = [
       '#type' => 'radios',
       '#title' => $this->t('Choose one'),
@@ -77,6 +79,7 @@ class THMDonationOptions extends CheckoutPaneBase {
     $pane_form['donor_email'] = [
       '#type' => 'email',
       '#title' => $this->t('Email Address'),
+      '#default_value' => $currentUser->isAuthenticated() ? $currentUser->getEmail() : '',
       '#required' => true
     ];
 
