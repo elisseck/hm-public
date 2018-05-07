d3ae40ae9b4ab4f8c7e3079321d399f07077e4f6
August McKinney <august.mckinney@thirdwavellc.com>
2018-05-07 08:52:22 -0500

    Timeline - Updates from Jibran

--- web/modules/custom/thm_timeline/src/Controller/TimeLineController.php
+++ web/modules/custom/thm_timeline/src/Controller/TimeLineController.php
@@ -17,22 +17,20 @@ class TimeLineController {
         }
 
 
+
         $content = array();
         $pagination = '';
         $sql="";
-        $paginationBool = true;
+        $day=null; $month=null; $year=null;
+
+
         // Check connection
         if (mysqli_connect_errno()){  echo "Failed to connect to MySQL: " . mysqli_connect_error(); }
-         $page_number = isset($_GET['page']) ? $_GET['page'] : '1';
-         $page_offset = 20 * ($page_number -1);
-         $dateSelector = "";
+           $page_number = isset($_GET['page']) ? $_GET['page'] : '1';
+           $page_offset = 20 * ($page_number -1);
+           $dateSelector = "";
         if( isset($_GET['day']) || isset($_GET['month']) || isset($_GET['year'])){
-          $paginationBool = false;
-          $day = $_GET['day']; $month = $_GET['month']; $year = $_GET['year'];
-          // $day = empty($_GET['day']) ? "":"%/". $_GET['day'] ."/%";
-          // $month = empty($_GET['month']) ? "":  $_GET['month'] . "/%";
-          // $year = empty($_GET['year']) ? "": "%/". $_GET['year'];
-
+            $day = $_GET['day']; $month = $_GET['month']; $year = $_GET['year'];
             //month day year are set
           if( !empty($month) && !empty($day) && !empty($year)){
             $dateSelector = " = '$month/$day/$year'";
@@ -55,22 +53,33 @@ class TimeLineController {
           if (empty($dateSelector)){
                 $host  = $_SERVER['HTTP_HOST'];
                 $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-                $extra = 'timeline?page=1';
+                $page = isset($_GET['page']) ? $_GET['page'] : '1';
+                isset($_GET['page']) ?  $page = $_GET['page'] : $page = '1';
+                $extra = 'timeline?page='.$page;
                 header("Location: http://$host$uri/$extra");
                 exit;
           }
           else{
-             $sql= "SELECT @type := 'birth' as type, field_birth_date_value  as date, entity_id as nid
+             $query= "SELECT @type := 'birth' as type, field_birth_date_value  as date, entity_id as nid
              FROM `node__field_birth_date` WHERE field_birth_date_value $dateSelector
              UNION SELECT  @type := 'death' as type, field_death_date_value  as date,
              entity_id as nid FROM `node__field_death_date` WHERE field_death_date_value $dateSelector
              UNION SELECT @type := 'milestone' as type,
              field_timeline_year_value as date, entity_id as nid FROM `paragraph__field_timeline_year`
              WHERE field_timeline_year_value $dateSelector
-             ORDER BY DATE_FORMAT(STR_TO_DATE(date, '%m/%d/%Y'), '%Y-%m-%d') DESC";
-           }
+             ORDER BY DATE_FORMAT(STR_TO_DATE(date, '%m/%d/%Y'), '%Y-%m-%d')";
+
+             $limit = "DESC LIMIT 20 OFFSET $page_offset";
+             $sql = $query . $limit;
+
+
+             $resultsCount = getSQLData("SELECT COUNT( * ) as count FROM ($query) AS a")[0] -> count;
+
+
+          }
 
         }else{
+
             $sql = "SELECT @type := 'birth' as type, field_birth_date_value  as date, entity_id as nid
             FROM `node__field_birth_date` WHERE field_birth_date_value <> '?'
             UNION SELECT  @type := 'death' as type, field_death_date_value  as date,
@@ -78,9 +87,16 @@ class TimeLineController {
             UNION SELECT @type := 'milestone' as type, field_timeline_year_value as date, entity_id
             as nid FROM `paragraph__field_timeline_year` WHERE field_timeline_year_value <> '?'
             ORDER BY DATE_FORMAT(STR_TO_DATE(date, '%m/%d/%Y'), '%Y-%m-%d') DESC LIMIT 20 OFFSET $page_offset";
+            $resultsCount = getSQLData("SELECT COUNT( * ) as count FROM (SELECT * FROM `node__field_birth_date` WHERE field_birth_date_value <> '?'
+            UNION SELECT * FROM `node__field_death_date` WHERE field_death_date_value <> '?') AS a")[0] -> count;
+
         }
 
-        if(!empty($sql)){$rows = getSQLData($sql);}
+
+        if(!empty($sql)){
+          $rows = getSQLData($sql);
+
+        }
 
 
          if ($rows) {
@@ -89,6 +105,7 @@ class TimeLineController {
                    $nid = $row -> nid;
                    $date = $row -> date;
                    $type = $row -> type;
+
                    if($type == 'birth' || $type=="death"){
                        $name =  getSQLData("SELECT * from `node_field_data` WHERE `nid` = '$nid'")[0] -> title;
                        $file_id = getSQLData("SELECT * from `node__field_bio_image` WHERE `entity_id` = '$nid'")[0] -> field_bio_image_target_id;
@@ -110,6 +127,7 @@ class TimeLineController {
                    }
 
                    $final = array('date' => $date, 'type' => $type, 'nid' => $nid, 'name' => $name, 'img' => $img, 'bio' => $bio);
+
                    array_push($content, $final);
              }
           }
@@ -125,14 +143,12 @@ class TimeLineController {
                 return $data; //return the paging data
            }
 
-          function pagination($sql,$page_number) {
-               $count = getSQLData("SELECT COUNT( * ) as count FROM (SELECT * FROM `node__field_birth_date` WHERE field_birth_date_value <> '?'
-                 UNION SELECT * FROM `node__field_death_date` WHERE field_death_date_value <> '?') AS a")[0] -> count;
-               $paging_info = get_paging_info($count, 20, $page_number);
+          function pagination($sql,$page_number, $resultsCount, $day, $month, $year) {
+               $paging_info = get_paging_info($resultsCount, 20, $page_number);
                $html ="";
                 if($paging_info['curr_page'] > 1) {
                    $prevPage = $paging_info['curr_page'] - 1;
-                   $html .= "<li class='pager__item'><a href='?page=$prevPage' title='Page $prevPage'> << </a></li> ";
+                   $html .= "<li class='pager__item'><a href='?page=$prevPage&year=$year&month=$month&day=$day' title='Page $prevPage'> << </a></li> ";
                 }
 
                 $max = 7;
@@ -143,38 +159,44 @@ class TimeLineController {
                 elseif($paging_info['curr_page'] >= $max)
                 $sp = $paging_info['curr_page']  - floor($max/2);
 
-                if($paging_info['curr_page'] >= $max) {  $html .= " <li class='pager__item'><a href='?page=1' title='Page 1'>1 ..</a></li>"; }
+                if($paging_info['curr_page'] >= $max) {  $html .= " <li class='pager__item'><a href='?page=1&year=$year&month=$month&day=$day' title='Page 1'>1 ..</a></li>"; }
 
                 for($i = $sp; $i <= ($sp + $max -1);$i++)
                 {
                    if($i > $paging_info['pages']) { continue; }
                    if($paging_info['curr_page'] == $i){  $html .= "<li class='pager__item is-active'> <a href=''><span class='bold'>$i</span> </a>";}
-                   else{; $html .="<li class='pager__item'><a href='?page=$i' title='Page $i'> $i </a></li>";}
+                   else{; $html .="<li class='pager__item'><a href='?page=$i&year=$year&month=$month&day=$day' title='Page $i'> $i </a></li>";}
                 }
 
 
                 if($paging_info['curr_page'] < ($paging_info['pages'] - floor($max / 2))){
                      $page_info_pages = $paging_info['pages'];
-                     $html .= "<li class='pager__item'><a href='?page=$page_info_pages' title='Page $page_info_pages'>.. $page_info_pages </a></li>";
+                     $html .= "<li class='pager__item'><a href='?page=$page_info_pages&year=$year&month=$month&day=$day' title='Page $page_info_pages'>.. $page_info_pages </a></li>";
                 }
 
                 if($paging_info['curr_page'] < $paging_info['pages']){
                        $nextPage = $paging_info['curr_page'] + 1;
                        $lastPage = $paging_info['pages'];
-                       $html .= "<li class='pager__item'><a href='?page=$nextPage' title='Page $nextPage'> >> </a></li>";
+                       $html .= "<li class='pager__item'><a href='?page=$nextPage&year=$year&month=$month&day=$day' title='Page $nextPage'> >> </a></li>";
                 }
                 return $html;
             }
 
-             if($paginationBool){$pagination = pagination($sql, $page_number);}
+          $pagination = pagination($sql, $page_number, $resultsCount, $day, $month, $year);
 
-            return array(
+
+
+
+
+
+             return array(
               '#theme' => 'timeline',
               '#title' => 'headline',
+              '#resultsCount' => $resultsCount,
               '#markup' => "Browse or search our African American History timeline. Events on the timeline include key moments in specific HistoryMaker's lives as well as important historical dates.",
               '#content' => $content,
               '#pages' => $pagination,
-            );
+             );
 
 
 
