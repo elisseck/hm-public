6ce52ccc2ed30e73fae249383f4514eb50153cb5
Tony Taylor <tony.taylor@thirdwavellc.com>
2018-05-31 16:58:06 -0500

    advanced search:   - added filter removal logic (initial)   - added filter conversion (resolve taxonomy names to IDs and vice versa)   - added filter reset button

--- web/modules/custom/thm_adv_search/js/make-autocomplete-widget.js
+++ web/modules/custom/thm_adv_search/js/make-autocomplete-widget.js
@@ -3,31 +3,128 @@
     'use strict';
 
     Drupal.thm = Drupal.thm || {};
+
+    Drupal.thm.filtersApplied = false;
+
+    Drupal.thm.filterResetCls = '.thm-adv-search-filter-mgmt';
+
+    Drupal.thm.widgetCls = '.js-facets-autocomplete';
+
+    Drupal.thm.taxonomyWidgets = [ 'birth_place_term' ];
+
+    Drupal.thm.excludeWidgets = [ 'maker_category' ];
+
+    Drupal.thm.handleFilterReset = function(settings) {
+        $('#filter-reset').on('click', function(evt) {
+            evt.preventDefault();
+            window.location.replace(settings.path.baseUrl + settings.path.currentPath);
+        });
+    };
+
     Drupal.behaviors.autoComplete = {
         attach: function autoComplete(context, settings) {
-            var domCls = '.js-facets-autocomplete';
-            [].forEach.call(document.querySelectorAll(domCls), _bind(settings));
+            $(Drupal.thm.widgetCls).once().each(_bindAutocomplete);
+
+            if (!Drupal.thm.filtersApplied) {
+                Drupal.thm.handleFilterReset(settings);
+                _applyFiltersInUrl(settings);
+            }
         }
     };
 
-    function _bind(settings) {
-        return function(ele) {
-            var widgetId = _getWidgetId(ele),
-                options = {
-                    getValue: function(e) { return e.name; },
-                    url: '/fetch-facet-data/' + widgetId,
-                    list: {
-                        match: { enabled: true },
-                        onClickEvent: function() { _applyFilter($(ele).getSelectedItemData()); }
-                    }
-                };
-            $(ele).easyAutocomplete(options);
+    function _bindAutocomplete() {
+        var ele      = $(this),
+            widgetId = _getWidgetId(ele),
+            options  = {
+                getValue: function(e) { return e.name; },
+                url: '/fetch-facet-data/' + widgetId,
+                list: {
+                    match: { enabled: true },
+                    onClickEvent: function() { _addFilter($(ele).getSelectedItemData()); }
+                }
+            };
+        $(ele).easyAutocomplete(options);
+    }
+
+    function _applyFiltersInUrl(settings) {
+        Drupal.thm.filtersApplied = true;
+
+        if (!settings.path.hasOwnProperty('currentQuery')) {
+            return false;
+        }
+
+        var filters = settings.path.currentQuery.f;
+
+        Object.keys(filters).forEach(function(filter) {
+            var args = filters[filter].split(':');
+
+            if (Drupal.thm.excludeWidgets.includes(args[0])) {
+                return;
+            } else if (Drupal.thm.taxonomyWidgets.includes(args[0])) {
+                _lookUpTerm(filters[filter]);
+            } else {
+                _createFilterControl({ name: args[1], field: args[0] });
+            }
+        });
+    }
+
+    function _createFilterControl(cfg) {
+        var ele = [
+                '<div data-facet-field-name="', cfg.field, '"',
+                'data-facet-field-value="', cfg.name, '"',
+                'data-facet-field-id="', cfg.id || 0, '">',
+                cfg.name, '</div>'
+            ].join(''),
+            $filterCtrl = $(ele);
+
+        $($filterCtrl).on('click', _removeFilter);
+        $(Drupal.thm.filterResetCls).append($filterCtrl);
+    }
+    
+    function _removeFilter(evt) {
+        var $ele   = $(this),
+            $data  = _extractDataFromFilterControl($ele),
+            url    = window.location.href,
+            entry  = '';
+
+        if ($data.id) {
+            entry = encodeURIComponent($data.field + ':' + $data.id);
+        } else {
+            entry = encodeURIComponent($data.field + ':' + $data.value);
+        }
+
+        var url = url.replace(new RegExp('^.+(f\[[0-9]+\]=' + entry + ')$'), _refreshUrl);
+        console.log('removing ', entry);
+        console.log('new url ', url)
+    }
+
+    function _extractDataFromFilterControl($ele) {
+        return {
+            id: $ele.data('facetFieldId'),
+            value: $ele.data('facetFieldValue'),
+            field: $ele.data('facetFieldName')
         }
     }
 
-    function _applyFilter(data) {
+    function _refreshUrl(match, entry, offset, string) {
+        var beginIdx = string.indexOf(entry),
+            endIdx   = match.length;
+        console.log(arguments);
+    }
+
+    function _lookUpTerm(arg) {
+        var args = arg.split(':');
+
+        fetch('/fetch-facet-data/' + args[0]).then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            var filterName = data.find(function(item) { return item.value.endsWith(args[1]); }).name;
+            _createFilterControl({ name: filterName, field: args[0], id: args[1] });
+        });
+    }
+
+    function _addFilter(data) {
         _parseUrl(window.location.href, data);
-        // TODO: Add selected filter to search-filter-mgmt block
     }
 
     function _parseUrl(url, data) {
@@ -45,9 +142,8 @@
     }
 
     function _getWidgetId(ele) {
-        var module  = 'thm_adv_search_';
-
-        var id = [].find.call(ele.classList, function(item) {
+        var module = 'thm_adv_search_';
+        var id = [].find.call(ele[0].classList, function(item) {
             return item.startsWith(module);
         });
 
