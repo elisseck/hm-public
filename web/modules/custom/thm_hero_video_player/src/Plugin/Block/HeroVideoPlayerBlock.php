14f7d3f311d6893a8ea58485ddd9efbadb73c6ec
Tony Taylor <tony.taylor@thirdwavellc.com>
2018-06-01 14:16:38 -0500

    hero video player:   - added block configuration form   - refactored block to pass config values (from form) to twig (via theme vars)   - refactored twig to use theme variables

--- web/modules/custom/thm_hero_video_player/src/Plugin/Block/HeroVideoPlayerBlock.php
+++ web/modules/custom/thm_hero_video_player/src/Plugin/Block/HeroVideoPlayerBlock.php
@@ -3,11 +3,7 @@
 namespace Drupal\thm_hero_video_player\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Routing\RouteMatchInterface;
-use Drupal\search_api\Entity\Index;
-use Drupal\node\NodeInterface;
-use Drupal\file\Entity\File;
-use Drupal\taxonomy\Entity\Term;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * @Block(
@@ -16,22 +12,54 @@ use Drupal\taxonomy\Entity\Term;
  *   category = @Translation("THM Hero Video Player Block"),
  * )
  */
-
-
 class HeroVideoPlayerBlock extends BlockBase {
-  /**
-   * Retrieves the node in current display context (page|view|block).
-   * Returns false if node isn't found.
-   *
-   * @return \Drupal\node\Entity\Node|bool
-   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+
+    $config = $this->getConfiguration();
+
+    $form['hero_video_url'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Video URL'),
+      '#default_value' => isset($config['video_url']) ? $config['video_url'] : ''
+    ];
+
+    $form['hero_title'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Video Title'),
+      '#default_value' => isset($config['hero_title']) ? $config['hero_title'] : ''
+    ];
+
+    $form['hero_desc'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Video Description'),
+      '#default_value' => isset($config['hero_desc']) ? $config['hero_desc'] : ''
+    ];
+
+    return $form;
+  }
+
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->setConfigurationValue('video_url',
+      $form_state->getValue('hero_video_url'));
+    $this->setConfigurationValue('hero_title',
+      $form_state->getValue('hero_title'));
+    $this->setConfigurationValue('hero_desc',
+      $form_state->getValue('hero_desc'));
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    $build['thm_hero_video_player']['#theme'] = 'angular';
-    return $build;
+    $config = $this->getConfiguration();
+
+    return [
+      '#theme' => 'angular',
+      '#video' => $config['video_url'],
+      '#title' => $config['hero_title'],
+      '#body' => $config['hero_desc']
+    ];
   }
 
 }
