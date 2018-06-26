c3357e6242876b6bd5b472d97fb48c5cd201b6de
Tony Taylor <tony.taylor@thirdwavellc.com>
2018-06-26 16:12:20 -0500

    bio exports (and related):  - added tags to facet data  - added argument support to import (cli) script  - added full-process support to import (cli) script  - added cli progress bar to code base

--- web/cli_import.php
+++ web/cli_import.php
@@ -1,11 +1,83 @@
 <?php
 
-use Drupal\bio_import_xml\Helpers\BioXMLMigrationImporter;
+use Drupal\bio_import_xml\Helpers;
+
+const MODULE_NAME = 'bio_import_xml';
+const CONFIG_ID =  MODULE_NAME . '.settings';
 
 $db =  \Drupal::database();
-$cfg = \Drupal::config('bio_import_xml.settings');
-$log = \Drupal::logger('bio_import_xml');
+$cfg = \Drupal::service('config.factory');
+$log = \Drupal::logger(MODULE_NAME);
 
-\drupal_set_message('Importing biographies. . .');
+/**
+ * Loads command-line arguments into a Drupal configuration object
+ *
+ * @param $config \Drupal\Core\Config\ConfigFactory
+ *
+ * @return \Drupal\Core\Config\ConfigFactoryInterface
+ */
+function loadArgs($config) {
+  $cfg = $config->getEditable(CONFIG_ID);
 
-BioXMLMigrationImporter::import($db, $cfg, $log);
+  if ($xmlPath = drush_get_option('xml-path')) {
+    $cfg->set(MODULE_NAME . '.fm_path', $xmlPath);
+  }
+
+  if ($mediaPath = drush_get_option('media-path')) {
+    $cfg->set(MODULE_NAME . '.fm_files_path', $mediaPath);
+  }
+
+  if ($notify = drush_get_option('notify')) {
+    $cfg->set(MODULE_NAME . '.notify_email', $notify);
+  }
+
+  if (drush_get_option('reset')) {
+    $cfg->set(MODULE_NAME . '.rebuild_ingestion_table', true);
+  }
+
+  $cfg->save();
+
+  return $cfg;
+}
+
+function clean($config) {
+  $cleaned = Helpers\BioXMLMigrationCleaner::clean($config);
+
+  if ($cleaned) {
+    drupal_set_message(t('XML Document has been cleaned.'), 'status');
+  } else {
+    drupal_set_message('An error occurred. Please check the error logs.', 'error');
+  }
+}
+
+function ingest($database, $config) {
+  if ($config->get(MODULE_NAME . '.rebuild_ingestion_table')) {
+    drupal_set_message('all records will be marked new.', 'status');
+    Helpers\BioXMLMigrationIngestor::ingest($database, $config, true);
+  }
+}
+
+function import($database, $config, $logger) {
+  Helpers\BioXMLMigrationImporter::import($database, $config, $logger);
+}
+
+function help() {
+  drupal_set_message('it was written. . .');
+}
+
+function run($database, $config, $logger) {
+  drupal_set_message('preparing configuration');
+  $config = loadArgs($config);
+  drupal_set_message('executing sanitizing phase. . .');
+  clean($config);
+  drupal_set_message('executing ingestion phase. . .');
+  ingest($database, $config);
+  drupal_set_message('executing import');
+  import($database, $config, $logger);
+}
+
+if (drush_get_option('info')) {
+  help();
+} else {
+  run($db, $cfg, $log);
+}
