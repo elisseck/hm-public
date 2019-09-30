849876ebbbc4f45b6ff0b17fb5685a3cbbb21305
Orin Fink <orin.fink@thirdwavellc.com>
2019-09-29 21:39:42 -0500

    CiviCRM file must be kept as a shared (linked) resource

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ set :deploy_to, '/var/www/hm-public'
 set :composer_install_flags, '--no-dev --no-interaction --quiet --optimize-autoloader'
 
 append :linked_files, 'web/sites/default/settings.php'
+append :linked_files, 'web/sites/default/civicrm.settings.php'
 append :linked_dirs, 'web/sites/default/files'
 append :linked_dirs, 'vendor/civicrm/civicrm-core/packages'
 
@@ -27,7 +28,7 @@ namespace :drupal do
     on roles(:app), in: :sequence, wait: 5 do
       within release_path do
         execute 'vendor/drush/drush/drush', 'state:set system.maintenance_mode 1 --input-format=integer'
-        execute 'vendor/drush/drush/drush', 'cache:rebuild'
+
       end
     end
   end
@@ -37,7 +38,7 @@ namespace :drupal do
     on roles(:app), in: :sequence, wait: 5 do
       within release_path do
         execute 'vendor/drush/drush/drush', 'state:set system.maintenance_mode 0 --input-format=integer'
-        execute 'vendor/drush/drush/drush', 'cache:rebuild'
+
       end
     end
   end
