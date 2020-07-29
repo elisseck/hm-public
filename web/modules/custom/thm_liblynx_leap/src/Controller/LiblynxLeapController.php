55d9b26d15e028eb130bdb87695d6ae2ad4684b3
Thomas P. Sychay <thomas.sychay@thirdwavellc.com>
2020-07-29 15:58:23 -0500

    Match format of unknown user or incorrect password.

--- web/modules/custom/thm_liblynx_leap/src/Controller/LiblynxLeapController.php
+++ web/modules/custom/thm_liblynx_leap/src/Controller/LiblynxLeapController.php
@@ -39,41 +39,21 @@ class LiblynxLeapController extends ControllerBase {
 			$request->request->replace( is_array( $data ) ? $data : [] );
 		}
 
-    $targetHash=$data['hash'];
-    $sourceHash=$this->createHash($data['relyingParty'].$data['username'].$data['password'].$data['timestamp']);
-
-    $validRequest=($targetHash==$sourceHash) ? true : false;
-
-    if ($validRequest) {
-      $datetime1 = $gmdate;
-      $datetime2 = date_create($data['timestamp']);
-      $diff=date_diff($datetime1,$datetime2);
-      $validRequest=($diff->h <= 1) ? true : false;
+    // check username
+    $uidQuery = \Drupal::entityQuery('user')
+      ->condition('name', $data['username'])
+      ->range(0, 1)
+      ->execute();
+    if(!empty($uidQuery)){
+      $uid=array_pop(array_reverse($uidQuery));
     }
 
-    if ($validRequest) {
-      // check username
-      $uidQuery = \Drupal::entityQuery('user')
-        ->condition('name', $data['username'])
-        ->range(0, 1)
-        ->execute();
-      if(!empty($uidQuery)){
-        $uid=array_pop(array_reverse($uidQuery));
-      }
-
-      // retreive user information
-      if ($uid) {
-        $user = \Drupal\user\Entity\User::load($uid);
-        $roles = $user->getRoles();
-        $email = $user->getEmail();
-        $name = $user->getDisplayName();
-      }
-
-      // does password match?
-      $authenticatedUid = \Drupal::service('user.auth')->authenticate($data['username'], $data['password']);
-      if ($uid != $authenticatedUid) {
-        $authenticatedUid=false;
-      }
+    // retreive user information
+    if ($uid) {
+      $user = \Drupal\user\Entity\User::load($uid);
+      $roles = $user->getRoles();
+      $email = $user->getEmail();
+      $name = $user->getDisplayName();
 
       // get roles
       $is_thm_paid_member=array_search('thm_paid_member',$roles);
@@ -83,25 +63,57 @@ class LiblynxLeapController extends ControllerBase {
       }
     }
 
-    $gmdateString=date_format($gmdate,'Y-m-d\TH:i:s');
-    $responseHash=$this->createHash($uid.$gmdateString);
+    // does password match?
+    $authenticatedUid = \Drupal::service('user.auth')->authenticate($data['username'], $data['password']);
+    if ($uid != $authenticatedUid) {
+      $authenticatedUid=false;
+    }
 
-    $response['knownuser'] = ($uid != false) ? 'true' : 'false';
-    $response['validpassword'] = ($authenticatedUid != false) ? 'true' : 'false';
+    // check authenticty of request
+    $targetHash=$data['hash'];
+    $sourceHash=$this->createHash($data['relyingParty'].$data['username'].$data['password'].$data['timestamp']);
+    $validRequest=($targetHash==$sourceHash) ? true : false;
 
-    $account['id']='thm';
-    $account['name']='The HistoryMakers';
-    $response['account']=$account;
+    if ($validRequest) {
+      $datetime1 = $gmdate;
+      $datetime2 = date_create($data['timestamp']);
+      $diff=date_diff($datetime1,$datetime2);
+      $validRequest=($diff->d <= 1) ? true : false;
+    }
 
-    $returnUser['id']=$uid;
-    $metadata['email']=$email;
-    $metadata['name']=$name;
-    $returnUser['metadata']=$metadata;
-    $response['user']=$returnUser;
+    if ($validRequest==false) {
+      $validpassword=false;
+    }
 
-    $response['units']=$returnRoles;
-		$response['timestamp'] = $gmdateString;
-		$response['hash'] = $responseHash;
+    $knownuser=($uid != false) ? true : false;
+    $validpassword=($authenticatedUid != false) ? true : false;
+
+    $response['knownuser'] = $knownuser;
+    $response['validpassword'] = $validpassword;
+
+    if ($knownuser) {
+      if ($validpassword) {
+        $gmdateString=date_format($gmdate,'Y-m-d\TH:i:s');
+        $responseHash=$this->createHash($uid.$gmdateString);
+
+        $account['id']='thm';
+        $account['name']='The HistoryMakers';
+        $response['account']=$account;
+
+        $returnUser['id']=$uid;
+        $metadata['email']=$email;
+        $metadata['name']=$name;
+        $returnUser['metadata']=$metadata;
+        $response['user']=$returnUser;
+
+        $response['units']=$returnRoles;
+        $response['timestamp'] = $gmdateString;
+        $response['hash'] = $responseHash;
+      }
+      else {
+        $response['resetUrl']='https://www.thehistorymakers.org/user/password';
+      }
+    }
 
 		return new JsonResponse( $response );
 	}
