5f4c90c9c9c2216361384fd1cd0cbe65967ee09c
Orin Fink <orin.fink@thirdwavellc.com>
2021-05-01 13:56:31 -0500

    update capistrano config for production to use the data directory on the alternate volume

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,6 @@
 server "www.thehistorymakers.org", user: "devuser", roles: %w{web app db}
+
+
+set :repo_url, 'git@github.com:thirdwavellc/hm-public.git'
+set :branch, 'env/production'
+set :deploy_to, '/data/www/hm-public'
