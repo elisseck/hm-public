93a72c26af99163b59f6d0ba4ad8b8861438b40b
Orin Fink <orin.fink@thirdwavellc.com>
2021-05-05 18:26:53 -0500

    set up the TimingPair class a little smarter with a constructer and builder method.

--- web/themes/custom/hm-public-theme/bio-video-player/src/app/transcript-timing.ts
+++ web/themes/custom/hm-public-theme/bio-video-player/src/app/transcript-timing.ts
@@ -1,4 +1,19 @@
 export class TranscriptTiming {
-    public offset: number;
-    public time: number;
+  public offset: number;
+  public time: number;
+
+  constructor() {
+    this.offset = 0;
+    this.time = 0;
+  }
+
+  buildFromArray(timingPairArray){
+    let myTime = Number(timingPairArray[0]);
+    let myOffset = Number(timingPairArray[1]);
+    if(!isNaN(myTime))
+      this.time = myTime;
+    if(!isNaN(myOffset))
+      this.offset = myOffset;
+    return this;
+  }
 }
